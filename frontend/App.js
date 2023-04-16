0614f8761599af39d72f562d606843008723bede
aufung1999 <78595669+aufung1999@users.noreply.github.com>
2023-04-16 16:36:14 -0400

    update

--- frontend/App.js
+++ frontend/App.js
@@ -1,18 +1,22 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { Provider } from "react-redux";
+import "react-native-gesture-handler";
 
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import reducers from "./Redux/reducers";
 
-import Reactotron from './ReactotronConfig'
+import Reactotron from "./ReactotronConfig";
 
-if(__DEV__) {
-  import('./ReactotronConfig').then(() => console.log('Reactotron Configured'))
+if (__DEV__) {
+  import("./ReactotronConfig").then(() => console.log("Reactotron Configured"));
 }
 
-export const store = createStore(reducers, compose(applyMiddleware(thunk), Reactotron.createEnhancer()) )
+export const store = createStore(
+  reducers,
+  compose(applyMiddleware(thunk), Reactotron.createEnhancer())
+);
 
 import MainNavigator from "./app/MainNavigator";
 import LoginProvider from "./app/context/LoginProvider";
--- frontend/app/MainNavigator.js
+++ frontend/app/MainNavigator.js
@@ -1,26 +1,27 @@
-import React, { useContext } from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import React, { useContext } from "react";
+import { createStackNavigator } from "@react-navigation/stack";
 
-import AppForm from './components/login/AppForm';
-import ImageUpload from './components/login/ImageUpload';
-import UserProfile from './components/login/UserProfile';
-import { useLogin } from './context/LoginProvider';
-import DrawerNavigator from './DrawerNaviagtor';
+import AppForm from "./components/login/AppForm";
+import ImageUpload from "./components/login/ImageUpload";
+import UserProfile from "./components/login/UserProfile";
+import { useLogin } from "./context/LoginProvider";
+import DrawerNavigator from "./DrawerNaviagtor";
 
 const Stack = createStackNavigator();
 
 const StackNavigator = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen component={AppForm} name='AppForm' />
-      <Stack.Screen component={ImageUpload} name='ImageUpload' />
-      <Stack.Screen component={UserProfile} name='UserProfile' />
+      <Stack.Screen component={AppForm} name="AppForm" />
+      <Stack.Screen component={ImageUpload} name="ImageUpload" />
+      <Stack.Screen component={UserProfile} name="UserProfile" />
     </Stack.Navigator>
   );
 };
 
 const MainNavigator = () => {
   const { isLoggedIn } = useLogin();
+  isLoggedIn && console.log(" ITSSSSSSSSSSSSSSS HEREEEEEEEEEEEEEEEEEEEEEEE");
   return isLoggedIn ? <DrawerNavigator /> : <StackNavigator />;
 };
 export default MainNavigator;
